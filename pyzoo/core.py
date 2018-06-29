159b2d44ea9b74062f25db9389aeabe2693f9df0
chungheng <chungheng.yeh@gmail.com>
2018-06-29 17:07:04 -0400

    add draft implementation of zoo-adam

---
+++ pyzoo/core.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+class ZOOADAM(object):
+    class ADAM(object):
+        def __init__(self, dim, eta=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8):
+            self.T = np.zeros(dim, dtype=np.int)
+            self.M = np.zeros(dim)
+            self.v = np.zeros(dim)
+            self.eta = eta
+            self.beta1 = beta1
+            self.beta2 = beta2
+            self.epsilon = epsilon
+
+        def update(self, index, gradient):
+            self.T[index] += 1
+            self.M[index] = self.beta1*self.M[index] + (1.-self.beta1)*gradient
+            self.v[index] = self.beta2*self.v[index] + (1.-self.beta2)*gradient*gradient
+
+            M_hat = self.M[index] / (1.-pow(self.beta1, self.T[index]))
+            v_hat = self.v[index] / (1.-pow(self.beta2, self.T[index]))
+
+            delta = -self.eta*M_hat / (np.sqrt(v_hat)+self.epsilon)
+            return delta
+
+    def __init__(self, func, bounds, x0=None, step=0.001, maxiter=1000, disp=False):
+        self.maxiter = maxiter
+        self.dim = len(bounds)
+        self.func = func
+        self.bounds = np.array(bounds, dtype='float').T
+        self.basis = np.eye(self.dim)
+        self.step = step
+
+        if (np.size(self.bounds, 0) != 2 or not
+                np.all(np.isfinite(self.bounds))):
+            raise ValueError('bounds should be a sequence containing '
+                             'real valued (min, max) pairs for each value'
+                             ' in x')
+
+        self.__scale_arg1 = 0.5 * (self.bounds[0] + self.bounds[1])
+        self.__scale_arg2 = np.fabs(self.bounds[0] - self.bounds[1])
+
+        if x0 is not None:
+            self.__unscaled_x = self._unscale_parameters(x0)
+        else:
+            self.__unscaled_x = np.random.rand(self.dim)
+
+        self.disp = disp
+
+        self.adam = ZOOADAM.ADAM(self.dim)
+
+    @property
+    def x(self):
+        """
+        Return scaled x
+        """
+        return self._scale_parameters(self.__unscaled_x)
+
+    def update(self):
+
+        index = np.random.randint(self.dim)
+        gradient = self.estimate_gradient(index)
+        delta = self.adam.update(index, gradient)
+
+        self.__unscaled_x[index] += delta
+
+    def solve(self):
+        """
+        """
+        for i in xrange(self.maxiter):
+            self.update()
+            if self.disp:
+                print "[Error] Plus: %.5f Minus: %.5f" % (self.__f_plus, self.__f_minus)
+
+    def estimate_gradient(self, index):
+        perturbation = self.basis[index]*self.step
+        x_plus = self._scale_parameters(self.__unscaled_x + perturbation)
+        x_minus = self._scale_parameters(self.__unscaled_x - perturbation)
+
+        self.__f_plus = self.func(x_plus)
+        self.__f_minus = self.func(x_minus)
+
+        gradient = (self.__f_plus - self.__f_minus) / (2.*self.step)
+
+        return gradient
+
+    def _scale_parameters(self, trial):
+        """
+        scale from a number between 0 and 1 to parameters.
+        """
+        return self.__scale_arg1 + (trial - 0.5) * self.__scale_arg2
+
+    def _unscale_parameters(self, parameters):
+        """
+        scale from parameters to a number between 0 and 1.
+        """
+        return (parameters - self.__scale_arg1) / self.__scale_arg2 + 0.5
